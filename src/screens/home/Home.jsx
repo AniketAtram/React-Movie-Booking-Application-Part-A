55f9ca6773b2df222bfeeda10dae5af88b41379a
Aniket Atram <60972814+AniketAtram@users.noreply.github.com>
2022-04-01 13:29:01 +0530

    Added sub-heading underneath Header component

--- src/screens/home/Home.jsx
+++ src/screens/home/Home.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { Component, Fragment } from "react";
 import Header from '../../common/header/Header';
+import './Home.css';
 class Home extends Component {
     render() {
         return (
             <Fragment>
+                {/* Primary header of the app */}
                 <Header />
+                {/* Sub heading under the header */}
+                <div className="sub-heading" key="sub-heading">
+                    <span key="upcoming-movies">Upcoming Movies</span>
+                </div>
             </Fragment>
         )
     }
--- src/screens/home/ImageList.jsx
+++ src/screens/home/ImageList.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Component, Fragment } from 'react';
 import { ImageList, ImageListItem, ImageListItemBar, makeStyles } from '@material-ui/core';
-
+import moviesData from '../../assets/moviesData.json';
 class ImageListComponent extends Component {
     render() {
         return (
