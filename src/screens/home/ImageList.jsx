0fcf20d7ec3aedf0cdc31cbf12e089df7f5e57ea
Aniket Atram <60972814+AniketAtram@users.noreply.github.com>
2022-04-01 13:20:58 +0530

    Created a image list component

---
+++ src/screens/home/ImageList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Component, Fragment } from 'react';
+import { ImageList, ImageListItem, ImageListItemBar, makeStyles } from '@material-ui/core';
+
+class ImageListComponent extends Component {
+    render() {
+        return (
+            <Fragment>
+                
+            </Fragment>
+        ) 
+    }
+}
+
+export default ImageListComponent;
