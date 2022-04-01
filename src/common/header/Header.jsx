635046b51e1c6dc39d1e771da1f1abca4e9cbb2e
Aniket Atram <60972814+AniketAtram@users.noreply.github.com>
2022-04-01 12:48:07 +0530

    Created Header component

---
+++ src/common/header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Component, Fragment } from "react";
+import logo from '../../assets/logo.svg';
+import './Header.css';
+class Header extends Component {
+    render() {
+        return (
+            <Fragment>
+                <div className="header">
+                    <img src={logo} alt='website-logo' key="website-logo" className="website-logo"/>
+                </div>
+            </Fragment>
+        )
+    }
+}
+
+export default Header;
