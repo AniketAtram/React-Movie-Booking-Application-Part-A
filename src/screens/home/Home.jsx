42c839143b526ee823b8349279595a52023fed33
Aniket Atram <60972814+AniketAtram@users.noreply.github.com>
2022-04-07 15:05:11 +0530

    Decoupled the components

--- src/screens/home/Home.jsx
+++ src/screens/home/Home.jsx
@@ -3,7 +3,6 @@ import { Component, Fragment } from "react";
 import Header from '../../common/header/Header';
 import './Home.css';
 import moviesData from "../../assets/moviesData";
-import ImageListComponent from "./ImageList";
 class Home extends Component {
     constructor() {
         super();
@@ -12,22 +11,13 @@ class Home extends Component {
         }
     }
 
-    onClickHandler = () => {
-        alert('Redirecting to details page!');
-    }
     render() {
         return (
             <Fragment>
                 {/* Primary header of the app */}
                 <Header />
                 {/* Sub heading under the header */}
-                <div className="sub-heading" key="sub-heading">
-                    <span key="upcoming-movies">Upcoming Movies</span>
-                </div>
-                {/* The single line image list */}
-                <div className="single-line-image-list">
-                    <ImageListComponent moviesData={this.state.movieList} />
-                </div>
+                
                 {/* flex-container */}
                 <div className="flex-container">
                 
